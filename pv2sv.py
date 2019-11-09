6ce1c8e5c89bf4b38b83ca26351c42b720e877ef
Dustin Kendall <dkendall100@gmail.com>
2019-11-09 16:22:30 -0600

    Added predictive data collection in detect.py

--- pv2sv.py
+++ pv2sv.py
@@ -1,15 +1,76 @@
 import numpy as np
 import json
+import torch
 
 
 class StateVector:
 
-    def __init__(self, pred_model):
+    def __init__(self, x_center, y_center,DT):
         self.last_ball = None
-        self.last_zero = None # Predictive model network
+        self.last_zero = None  #Predictive model network
+        self.x_center = x_center
+        self.y_center = y_center
+        self.DT = DT
         self.b = 0
         self.z = 0
         self.i = 0
+
+    def get_tensor(self, det):
+
+        ball_flag = False
+        zero_flag = False
+        frame_state_vector = []
+        if len(det) > 0:
+            for inst in det:
+                if inst["cls"] == 0:
+                    ball = inst
+                    ball_flag = True
+                elif inst["cls"] == 1:
+                    zero = inst
+                    zero_flag = True
+                else:
+                    assert False, "Malformed class type in json array, class should be 0 or 1 only"
+            self.i = self.i + 1  # increment total count
+
+            #  Calculate ball speeds and accelerations
+            if self.last_ball is not None and ball_flag:
+                ball = StateVector.to_polar(ball, self.x_center, self.y_center)
+                ball['w'] = (StateVector.rad_dist(ball['theta'], self.last_ball['theta']) / (self.DT*(self.i - self.b)))
+                if self.last_ball['w'] is not None:
+                    ball['a'] = ((ball['w'] - self.last_ball['w']) / (self.DT*(self.i - self.b)))
+
+                if ball['a'] and ball['w'] is not None:
+                    frame_state_vector.append(ball)
+
+                self.last_ball = ball
+                self.b = self.b + 1  # increment ball detection count
+            elif self.last_ball is None and ball_flag:
+                self.last_ball = StateVector.to_polar(ball, self.x_center, self.y_center)
+                self.b = self.b + 1  # increment ball detection count
+
+            #  Calculate zero speeds and accelerations
+            if self.last_zero is not None and zero_flag:
+                zero = StateVector.to_polar(zero, self.x_center, self.y_center)
+                zero['w'] = ((StateVector.rad_dist(zero['theta'], self.last_zero['theta'])) / (self.DT*(self.i - self.z)))
+                if self.last_zero['w'] is not None:
+                    self.last_zero['a'] = ((zero['w'] - self.last_zero['w']) / ((self.i - self.z)*self.DT))
+                if zero['a'] and zero['w'] is not None:
+                    frame_state_vector.append(zero)
+                self.last_zero = zero
+                self.z = self.z + 1
+            elif self.last_zero is None and zero_flag:
+                self.last_zero = StateVector.to_polar(zero, self.x_center, self.y_center)
+                self.z = self.z + 1
+        else:
+            self.i = self.i + 1
+
+        if len(frame_state_vector) == 2:
+            return torch.tensor([[frame_state_vector[0]['r'], frame_state_vector[0]['theta'], frame_state_vector[0]['w'], frame_state_vector[0]['a']],
+                                 [frame_state_vector[1]['r'], frame_state_vector[1]['theta'], frame_state_vector[1]['w'], frame_state_vector[1]['a']]])
+        else:
+            return None
+
+
 
     @staticmethod
     def pv2sv(filein, fileout, x_center, y_center, result, DT):
