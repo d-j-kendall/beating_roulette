4833894aca89407172528aa1a3776bee681abe35
Dustin Kendall <dkendall100@gmail.com>
2019-11-17 14:04:38 -0600

    Added anode network with data loaders

--- predict/Models.py
+++ predict/Models.py
@@ -12,6 +12,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from random import shuffle
 import glob
+from anode.models import *
+from anode.training import *
+from predict.Data import *
+import torch.utils.data as data
 
 class Prediction(nn.Module):
 
@@ -31,5 +35,34 @@ class Prediction(nn.Module):
         return x
 
 
-class AnodePrediction(nn.Module):
-    pass
+class AnodePrediction:
+
+    def __init__(self, trained_params=""):
+        self.ode = ODENet(device=torch.device('cuda'),
+                          data_dim=8,
+                          hidden_dim=10,
+                          output_dim=1,
+                          augment_dim=1,
+                          non_linearity='relu',
+                          adjoint=True)
+
+        self.optimizer = torch.optim.Adam(self.ode.parameters(), lr=1e-3)
+        self.trained_params = trained_params
+        self.trainer = Trainer(self.ode,self.optimizer, torch.device('cuda'))
+        self.data_loader = None
+
+        if len(trained_params)>0:
+            self.ode.load_state_dict(torch.load(self.trained_params))
+
+    def train(self, epochs=10):
+        self.trainer.train(self.data_loader, epochs)
+        torch.save(self.ode, self.trained_params)
+
+    def set_data_loader(self, training_set):
+        self.data_loader = data.DataLoader(StateData(training_set), batch_size=20, shuffle=True, num_workers=20)
+
+
+
+
+
+
