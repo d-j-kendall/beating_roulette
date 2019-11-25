873bebd313288700d411e6a3d92c0d4adc4316f9
Dustin Kendall <dkendall100@gmail.com>
2019-11-24 22:27:08 -0600

    Revert "Revert "Made modifications to the training file""
    This reverts commit e71d327a40bea59df494872c52ace027b81d9c97.

--- predict/train.py
+++ predict/train.py
@@ -1,19 +1,19 @@
 import anode
 import torch
 from torch.utils.data import dataloader
-from predict.Data import StateData
-from predict.Models import AnodePrediction
+from predict.Data import StateData, StateData2
+from predict.Models import AnodePrediction, ConvAnodePrediction, ReducedAnodePrediction
 
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
-data_set = StateData('../training_data2.txt')
+data_set = StateData2('../training_data.txt')
 
-data_loader = dataloader.DataLoader(data_set, batch_size=10, shuffle=True, num_workers=0)
+data_loader = dataloader.DataLoader(data_set, batch_size=120, shuffle=True, num_workers=0)
 
-model = AnodePrediction('first_run.pd')
+model = ReducedAnodePrediction('reduced.pd')
 
 model.set_data_loader(data_loader)
 
-model.train(100)
+model.train(300)
 
 
 
